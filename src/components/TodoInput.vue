7dac1998b5141cc2e26a5e87e02abf2b6dcffc85
username <user@emil.mail>
2020-11-23 14:18:56 +0900

    Modal과 transition 작업 추가

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -4,21 +4,40 @@
     <span class="addContainer" @click="onAddTodo()">
       <i class="fa fa-plus addBtn"></i>
     </span>
+
+    <modal v-if="showModal" @close="showModal = false">
+      <h3 slot="header">
+        경고!
+        <i class="fa fa-times closeModalBtn" @click="showModal = false"></i>
+      </h3>
+      <div slot="body">
+        내용이 비어있습니다. 내용을 입력하세요.
+      </div>
+
+    </modal>
   </div>
 </template>
 
 <script>
+import Modal from './common/Modal'
+
 export default {
   data(){
     return {
-      newTodoText: ""
+      newTodoText: "",
+      showModal: false
     }
+  },
+  components:{
+    Modal
   },
   methods:{
     onAddTodo(){
       if(this.newTodoText !== ''){
         this.$emit('addTodoItem', this.newTodoText)
         this.onClearInput();
+      }else{
+        this.showModal = !this.showModal;
       }
     },
     onClearInput(){
@@ -52,5 +71,8 @@ input:focus{
 .addBtn{
   color: white;
   vertical-align: middle;
+}
+.closeModalBtn{
+  color: #42b983;
 }
 </style>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,14 +1,15 @@
 <template>
   <div>
-    <ul>
-      <li v-for="(todo, i) in todoList" :key="todo" class="shadow">
+    <transition-group name="list" tag="ul">
+      <li v-for="(todo, i) in todoList" :key="todo.item" class="shadow">
         <i class="checkBtn fa fa-check" :class="{'checkBtnCompleted': todo.completed}" @click="onToggleComplete(todo, i)"></i>
         <span :class="{'textCompleted': todo.completed}">{{todo.item}}</span>
         <span class="removeBtn" @click="onRemoveTodo(todo, i)">
           <i class="fa fa-trash-alt"></i>
         </span>
       </li>
-    </ul>
+    </transition-group>
+    
   </div>
 </template>
 
@@ -17,10 +18,10 @@ export default {
   props: ['todoList'],
   methods:{
     onToggleComplete(todo, index){
-      this.$emit('toggleItem', todo, index)
+      this.$emit('toggleItem', todo.item, index)
     },
     onRemoveTodo(todo, index){
-      this.$emit('removeItem', todo, index)
+      this.$emit('removeItem', todo.item, index)
     }
   }
 }
@@ -58,5 +59,13 @@ li{
 .removeBtn{
   margin-left: auto;
   color: #de4343;
+}
+/* 리스트 트렌지션 */
+.list-enter-active, .list-leave-active {
+  transition: all 1s;
+}
+.list-enter, .list-leave-to {
+  opacity: 0;
+  transform: translateY(30px);
 }
 </style>
